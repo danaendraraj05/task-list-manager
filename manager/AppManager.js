1c4fb3ac593e8fc85cc1af5c20af1629c784950e
Danaendraraj R <danaendraraj5@gmail.com>
2025-08-18 06:02:26 +0000

    Setup AppManager, constants

--- manager/AppManager.js
+++ manager/AppManager.js
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AddTaskScreen from '../screens/AddTaskScreen';
+import HomeScreen from '../screens/HomeScreen';
+import { AppScreens } from '../utils/constants';
 
 const AppManager = () => {
+  const [CurrentScreen, setCurrentScreen] = useState(AppScreens.HomeScreen);
   return (
     <View>
       <Text>AppManager</Text>
+      {CurrentScreen === AppScreens.AddTaskScreen ? ( 
+        <AddTaskScreen/>)
+        :
+        (<HomeScreen/>
+      )}
     </View>
   )
 }
---
+++ utils/constants.js
@@ -0,0 +1,4 @@
+export const AppScreens = {
+    HomeScreen : "HomeScreen",
+    AddTaskScreen : "AddTaskScreen"
+}
